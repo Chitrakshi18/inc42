d7053b6aa86eb2e0bb86633f1a23d9f57f0714e0
Chitrakshi <chitrakshipuri18@gmail.com>
2024-06-22 15:16:20 +0530

    feat: dockerize go app and add basic HTTP endpoint
    Signed-off-by: Chitrakshi <chitrakshipuri18@gmail.com>

--- go/main.go
+++ go/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello from Go App!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		message := map[string]string{"message": "Hello from Go API!"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(message)
+	})
+
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		message := map[string]string{"message": "Hello World endpoint"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(message)
+	})
+
+	port := ":8080"
+	fmt.Printf("Starting server on port %s\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		panic(err)
+	}
 }
